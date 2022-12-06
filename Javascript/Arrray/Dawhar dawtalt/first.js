a880a83af46dc9afcaad11d7d5389a5c0957977f
B Munkhsvld <bmunkhsvld@Bs-MacBook-Air.local>
2022-12-06 22:04:54 +0800

    homework

--- Javascript/Arrray/Dawhar dawtalt/first.js
+++ Javascript/Arrray/Dawhar dawtalt/first.js
@@ -1,6 +1,7 @@
-//gurwaljin
+
 let n = 5;
 let output = "";
+//gurwaljin
 for (let row = 1; row <= n; row++) {
   for (let col = 1; col <= n; col++) {
     if (col <= row) {
@@ -11,16 +12,17 @@ for (let row = 1; row <= n; row++) {
   debugger;
 }
 console.log(output);
-
 //gurwaljin
-
-for (let row = 1; row >= n; row--) {
-  for (let col = 1; col >= n; col--) {
-    if (col >= row) {
-      output += col + " ";
+let output1 = "";
+for (let row1 = 5; row1 >= 1; row1--) {
+  for (let col1 = 1; col1 <= n; col1++) {
+    if (col1 <= row1) {
+      output1 += col1 + " ";
     }
   }
-  output += "\n";
+  output1 += "\n";
   debugger;
 }
-console.log(output);
+console.log(output1);
+
+
--- Javascript/function/Array sorter function/function.js
+++ Javascript/function/Array sorter function/function.js
@@ -1,8 +1,13 @@
 arr = [4, 32, 2, 5, 8, -10, 20, 1, 0, -20];
+let arrNum = arr[0];
 
 function arraySorter(num) {
-  for (i = 0; arr.length > i; i++) {
-    console.log(arr);
+  for (i = 1; arr.length > i; i++) {
+    if(arrNum < arrNum[i]){
+      arrNum = arrNum[i];
+    }
+    console.log(arrNum)
   }
   return arraySorter;
 }
+
